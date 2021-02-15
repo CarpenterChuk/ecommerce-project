6beee4b709827825d5ff1116c2ef7c0fc970ed31
Vladislav Stoliarchuk <stoliarchuk.vlad@gmail.com>
2021-02-15 14:49:17 +0200

    Add products

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -122,4 +122,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'ecommerce/static/'
+]
+
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
