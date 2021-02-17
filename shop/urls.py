732ad82d9acc6786b73b06aa434a90457cb27c90
Vladislav Stoliarchuk <stoliarchuk.vlad@gmail.com>
2021-02-17 14:42:10 +0200

    Add detailed information about the product from the database

--- shop/urls.py
+++ shop/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('<slug:category_slug>', views.home, name='products_by_category'),
-    path('product/', views.product, name='product'),
+    path('<slug:category_slug>/<slug:product_slug>', views.product, name='product_detail'),
 ]
 
