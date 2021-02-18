976788427aa2be0486432eeecbfcce708dad7582
Vladislav Stoliarchuk <stoliarchuk.vlad@gmail.com>
2021-02-18 13:07:08 +0200

    Add model for cart

--- shop/urls.py
+++ shop/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('category/<slug:category_slug>', views.home, name='products_by_category'),
     path('category/<slug:category_slug>/<slug:product_slug>', views.product, name='product_detail'),
-    path('cart', views.cart, name='cart'),
+    path('cart', views.cart_detail, name='cart'),
 ]
 
