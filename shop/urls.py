210f50153485e68f6f75908584fb80cfa31a0953
Vladislav Stoliarchuk <stoliarchuk.vlad@gmail.com>
2021-03-09 18:37:14 +0200

    Add user registration

--- shop/urls.py
+++ shop/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('cart/add/<int:product_id>', views.add_cart, name='add_cart'),
     path('cart/remove/<int:product_id>', views.cart_remove, name='cart_remove'),
     path('cart/remove_product/<int:product_id>', views.cart_remove_product, name='cart_remove_product'),
+    path('account/create/', views.sign_up_view, name='signup'),
 ]
 
